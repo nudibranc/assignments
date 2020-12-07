2de8ddacbc32574de3529e93e95308fd77714331
clsandoval <clsandoval@up.edu.ph>
2020-12-07 17:38:50 +0800

    try for > 98

--- assignment1/mynet/kaggle.py
+++ assignment1/mynet/kaggle.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib as plt
 from matplotlib.pyplot import *
 from matplotlib import pyplot as plt
-from mynet import TwoLayerNet
+from twonet import MyTwoNet
 import pandas as pd
 
 #get dataset
@@ -13,16 +13,39 @@ y_val = None
 X_test = None
 y_test = None
 
+#predict the data
+final_x = np.array(pd.read_csv('/home/carlos/Downloads/test.csv', sep=',',dtype = np.uint8))
+print(final_x.shape)
+
 #X training data
 data = np.array(pd.read_csv('/home/carlos/Downloads/train.csv', sep=',',dtype = np.uint8))
 labels = data[:,0]
 X_train = data[:, 1:]
-print(X_train.shape)
-print(labels[0])
-imshow(X_train.reshape(-1,28,28)[0])
-plt.show()
 
+#split for testing
+X_test = X_train[40000:,:]
+y_test = labels[40000:]
+X_train = X_train[:40000,:]
+y_train = labels[:40000]
+print(X_train.shape)
+print(labels.shape)
+
+
+#initialize hyper hyper parameters
 input_size = 28*28
 num_classes = 10
-hidden_layers = 300
+hidden_size = 300
+
+#train
+mynet = MyTwoNet(input_size,hidden_size,num_classes)
+mynet.train(X_train.reshape(-1,28*28),labels,X_train.reshape(-1,28*28),labels, num_iters=10000, verbose= False)
+
+#predict
+y_pred = np.argmax(mynet.loss(final_x.reshape(-1,28*28)),axis=1)
+
+#print
+for i in range(28000):
+    print(f"{str(i+1)},{y_pred[i]}")
+imshow(final_x.reshape(-1,28,28)[27999])
+plt.show()
 
